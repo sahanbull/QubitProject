6b7ac6f5b5535f0cf1812cca0eb6113bd7dffd37
Sahanbull <sahanbull@gmail.com>
2013-06-06 12:49:02 +0100

    added some new features for filtering function

--- qbPreprocess.py
+++ qbPreprocess.py
@@ -3,6 +3,8 @@
 import csv
 import glob
 
+import qbGlobals as qbGbl
+
 ## this function returns all the file paths with .csv extension in the directory
 def listFiles(path):
 	path = '{0}\\*.csv'.format(path);
@@ -54,6 +56,27 @@ def writeFilCSV(file,filData):
 	for row in filData:
 		realFile.writerow(row); # write to file
 
+## this function conversts the string of classification to useable list
+def convClasses(field,split):
+	temp = field.split(split)
+	return temp;
+
+## this function conversts the string of classification to integer refs in the classDict
+def conv2ClsDict(field):
+	newField = [];
+	for tmpCls in field:
+		newField.append(qbGbl.classDict[tmpCls]);
+
+	return newField
+	
+## this function generates the dictionary of classes and their indexes
+def genClassDict(field,index):
+	for tmpCls in field:
+		if tmpCls not in qbGbl.classDict:
+			qbGbl.classDict[tmpCls] = index;
+			index+=1; 
+	return index
+
 ## this funciton imports the specified data set given in the string
 def importFilCSV(file):
 
@@ -65,21 +88,18 @@ def importFilCSV(file):
 
 	# reads the csv content
 	realFile = csv.reader(csvFile, delimiter=',');
-			
+	
+	index = 1;
 	for row in realFile:
+		# segment multiple classes
+		row[3]=convClasses(row[3],'|');
+		index = genClassDict(row[3],index);
 
+		row[3] = conv2ClsDict(row[3])
 		# 0: ref ID
 		# 1: worker ID
 		# 2: feedback content
 		# 3: classified classes
 		filData.append(row);
-
-	return filData
-
-## this function conversts the string of classification to useable list
-def convClasses(filData,split):
-	for row in filData:
-		temp = row[3].split(split)
-		row[3] = temp;
-
+	
 	return filData
