e085e4c62d72cabab7d61911cb1c33a17af39046
Sahanbull <sahanbull@gmail.com>
2013-06-06 21:15:00 +0100

    Added score file global var

--- qbGlobals.py
+++ qbGlobals.py
@@ -1,5 +1,5 @@
 
-# global variables
+################### global variables #######################
 
 global OriFileName;
 OriFileName = 'data\\read';
@@ -7,5 +7,10 @@ OriFileName = 'data\\read';
 global filFileName;
 filFileName = 'data\\write\\fil_comb_results.csv';
 
+# where to record worker scorecards
+global filFileName;
+scoreFileName = 'data\\write\\worker_scorecard.csv'
+
+# keeps the class references <class ref: ref index>
 global classDict;
 classDict = {};
