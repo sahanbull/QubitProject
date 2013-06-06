4170b831fcbde89dc1af5123ceade7a14e64e0d9
Sahanbull <sahanbull@gmail.com>
2013-06-06 12:47:49 +0100

    new globals py file added

---
+++ qbGlobals.py
@@ -0,0 +1,11 @@
+
+# global variables
+
+global OriFileName;
+OriFileName = 'data\\read';
+
+global filFileName;
+filFileName = 'data\\write\\fil_comb_results.csv';
+
+global classDict;
+classDict = {};
