53bd0cfbcbf8e36d07c85c6de413727991c6564b
Sahanbull <sahanbull@gmail.com>
2013-06-06 21:14:44 +0100

    minor modifications
    changed the x counter to boolean flag

--- qbPreprocess.py
+++ qbPreprocess.py
@@ -1,9 +1,12 @@
-######################## Preprocess data #########################
+
+####################### import pacakages #####################
 
 import csv
 import glob
 
 import qbGlobals as qbGbl
+
+######################## Preprocess data #########################
 
 ## this function returns all the file paths with .csv extension in the directory
 def listFiles(path):
@@ -22,26 +25,23 @@ def filterFile(file,count):
 	# reads the csv content
 	realFile = csv.reader(csvFile, delimiter=',');
 
-	x=0; # keeps local count of the iterator >> replaced by boolean flag
+	x = True; # keeps local count of the iterator >> replaced by boolean flag
 	#foreach row in the csv file
 	for row in realFile:
 		
 		# gets rid of the titles
-		if x==0:
-			x+=1; # count ++
+		if x:
+			x = False; # skip topic checked
 			continue;
 
 		# 15: worker ID
 		# 30: feedback content
 		# 31: classified classes
 		filData.append([count,row[15],row[30],row[31]]);
-		x+=1; # local cont ++
 		count+=1; # counter ++
-		if x>20:
-			break;
 
 	# return the filtered relevant data		
-	return [filData,x];
+	return [filData,count];
 
 ## this function writes the filtered dataset to a new file for operational convinience
 def writeFilCSV(file,filData):
@@ -58,7 +58,7 @@ def writeFilCSV(file,filData):
 
 ## this function conversts the string of classification to useable list
 def convClasses(field,split):
-	temp = field.split(split)
+	temp = field.split(split);
 	return temp;
 
 ## this function conversts the string of classification to integer refs in the classDict
@@ -68,7 +68,7 @@ def conv2ClsDict(field):
 		newField.append(qbGbl.classDict[tmpCls]);
 
 	return newField
-	
+
 ## this function generates the dictionary of classes and their indexes
 def genClassDict(field,index):
 	for tmpCls in field:
