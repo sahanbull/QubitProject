111ebb283be69f3a65f98fa8129fc01c967aeb8e
Sahanbull <sahanbull@gmail.com>
2013-06-07 19:03:21 +0100

    Added new global file name for 100 dataset

--- qbGlobals.py
+++ qbGlobals.py
@@ -7,10 +7,19 @@ OriFileName = 'data\\read';
 global filFileName;
 filFileName = 'data\\write\\fil_comb_results.csv';
 
+# record simple only dataset
+global filFileName;
+dataSet100 = 'data\\write\\dataSet_100.csv'
+
 # where to record worker scorecards
 global filFileName;
 scoreFileName = 'data\\write\\worker_scorecard.csv'
 
+# to store required attributes
+att = 'data\\attributes';
+
 # keeps the class references <class ref: ref index>
 global classDict;
 classDict = {};
+
+# preprocess the data 
