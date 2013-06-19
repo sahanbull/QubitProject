3bddd824980e1ca4045dbcbb6b7c4a1cf69bbe01
sahanbull <sahanbull@gmail.com>
2013-06-19 19:01:59 +0100

    Added TFIDF for x words

--- qbPrepare.py
+++ qbPrepare.py
@@ -17,19 +17,41 @@ def generateX(filData):
 	obs = len(filData);
 	dim = len(qbGbl.wordIDFDict)
 	
-	print obs;
-	print dim
+	# print obs;
+	# print dim
 	X = numpy.zeros([obs,dim]);
 
-	# for row in filData:
-	# 	tfidf = dict(Counter(row[1]));
-	# 	for word in tfidf:
-	# 		tfidf[word] = tfidf[word]*qbGbl.wordIDFDict[word];
-	# 		X[int(row[0]),word] = tfidf[word];
+	# foreach row in the dataset,
+	for row in filData:
+		# get the tf values of each word in the string
+		tfidf = dict(Counter(row[1]));
+
+		# foreach word in the row,
+		for word in tfidf:
+			# multiply by IDF for the word
+			tfidf[word] = tfidf[word]*qbGbl.wordIDFDict[word];
+
+			# place it in correct matrix observation
+			# X(obs ref,word ref)
+			X[int(row[0]),word] = tfidf[word];
 		
 	return X;	
 
+## this funciton generates the y vector for each class and produces a matrix with Ys
 def generateY(filData):
 	obs = len(filData);
-	# cls = len(qbGbl.)
+	# print qbGbl.classDict
+	cls = len(qbGbl.classDict)-1;
+	# generate zero matrix
+	Y = numpy.zeros([obs,cls]);
+
+	# foreach observation in dataset
+	for row in filData:
+		# foreach topic entry in observation,
+		for entry in row[2]:
+			# if the topic is present,
+			if entry in xrange(0,cls):
+				# mark that matrix location as 1
+				Y[int(row[0]),entry] = 1;
 	
+	return Y
