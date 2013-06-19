3b24c1fa953e144084009f806a1493461d1d2999
sahanbull <sahanbull@gmail.com>
2013-06-19 19:01:37 +0100

    added new file names

--- qbGlobals.py
+++ qbGlobals.py
@@ -1,38 +1,42 @@
 
 ################### global variables #######################
 
-oriFileName = 'data\\read';
+oriFileName = 'data/read';
 
-filFileName = 'data\\write\\fil_comb_results.csv';
+filFileName = 'data/write/fil_comb_results.csv';
 
 # record simple only dataset
-dataSetFileName = 'data\\write\\dataSet'
+dataSetFileName = 'data/write/dataSet'
 
-# where to record worker scorecards
-scoreFileName = 'data\\relAnalytics\\worker_scorecard.csv'
 
 # to store required attributes
-att = 'data\\attributes';
+att = 'data/attributes';
 
 # keeps the class references <class ref: ref index>
 classDict = {};
 
-clsDictFileName = '{0}\\classDict.csv'.format(att);
+clsDictFileName = '{0}/classDict.csv'.format(att);
 
 
 classUIRef = {};
 
-classUIRefFileName = '{0}\\classUIRef.csv'.format(att);
+classUIRefFileName = '{0}/classUIRef.csv'.format(att);
 
 
 wordRefDict = {};
 
-wordRefDictFileName = '{0}\\wordRefDict'.format(att);
+wordRefDictFileName = '{0}/wordRefDict'.format(att);
 
 
 wordIDFDict = {};
 
-wordIDFDictFileName = '{0}\\wordIDFDict'.format(att);
+wordIDFDictFileName = '{0}/wordIDFDict'.format(att);
+
+
+## datasets for analytics
+
+# where to record worker scorecards
+scoreFileName = 'data/relAnalytics/worker_scorecard.csv'
 
 
 ## lists and data items for visual analytics
