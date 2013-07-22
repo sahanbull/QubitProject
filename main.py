c402dbb829ff9c862fa1caab67ea36cbc8e86d59
sahanbull <sahanbull@gmail.com>
2013-07-22 12:32:45 +0100

    new addition of code for new classification

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 ####################### import pacakages #####################
 
 import numpy;
-import pandas as pd;
-import pylab as P
 import scipy
 import re
 import sklearn.cross_validation as cv
+import pandas as pd
+import pylab as P
 import sklearn
+
+import matplotlib.pyplot as plt
 
 import random
 
@@ -19,6 +21,21 @@ import qbPrepare as qbPrepare
 import test
 
 ########################## Main functions ########################
+
+def filterData2(cols=['HITId','HITTypeId','Title','Description','Keywords','Reward',
+	'CreationTime','MaxAssignments','RequesterAnnotation','AssignmentDurationInSeconds',
+	'AutoApprovalDelayInSeconds','Expiration','NumberOfSimilarHITs','LifetimeInSeconds',
+	'AssignmentId','WorkerId','AssignmentStatus','AcceptTime','SubmitTime','AutoApprovalTime',
+	'ApprovalTime','RejectionTime','RequesterFeedback','WorkTimeInSeconds','LifetimeApprovalRate',
+	'Last30DaysApprovalRate','Last7DaysApprovalRate','Input.pv_id','Input.global_user_id',
+	'Input.time','Input.declaration','Answer.Q1','Approve','Reject']):
+
+	paths = qbPre.listFiles(qbGbl.oriFileName)
+	filData = qbPre.readFiles(paths)
+
+	filData = filData[cols];
+
+	filData.to_csv(qbGbl.filFileName,index = False,encoding='utf-8');
 
 def filterData():
 	filData = []; # to store the filtered dataset
@@ -36,6 +53,105 @@ def filterData():
 
 	# write filtered data to a different file in the HDD 
 	qbPre.writeFilCSV(qbGbl.filFileName,filData);
+
+
+def analyseWorkers():
+	dataSet = qbRel.analyseWorkers();
+	workers = list(dataSet['WorkerId'].drop_duplicates())
+	
+	# print dataSet
+	# ax = plt.subplot(111)
+
+	## time wise aggregate for workers## time wise aggregate for workers
+
+	# sample = dataSet[['Time','WorkerId','AggrScore']]
+	# # foreach worker..
+	# for worker in workers:
+	# 	tempRecords = sample[sample.WorkerId==worker]
+	# 	plt.scatter(tempRecords.Time,tempRecords.AggrScore, hold='on')
+
+	# plt.title("Aggregated Score distribution by time of Day")
+	# plt.xlabel("Time of day in 24-hour")
+	# plt.xlim((-0.1, 24.1))
+	# plt.ylabel("Aggregated Score")
+	# plt.ylim((-0.1, 1.1))
+	# plt.show()
+
+	## timewise real Score for worker
+
+	# sample = dataSet[['Time','WorkerId','TempScore']]
+
+	# for worker in workers:
+	# 	tempRecords = sample[sample.WorkerId==worker]
+	# 	plt.scatter(tempRecords.Time,tempRecords.TempScore, hold='on')
+
+
+	# plt.title("Score distribution by time of Day")
+	# plt.xlabel("Time of day in 24-hour")
+	# plt.ylabel("Score")
+	# plt.show()
+				
+	# ## date wise agrr
+
+	# sample = dataSet[['Completion','WorkerId','AggrScore']]
+
+	# for worker in workers:
+	# 	tempRecords = sample[sample.WorkerId==worker]
+	# 	plt.plot(tempRecords.Completion,tempRecords.AggrScore, hold='on')
+
+	
+	# plt.title("Score distribution by Completion")
+	# plt.xlabel('percentage completed')
+	# plt.ylabel("Aggregated Score")
+	# plt.show()
+
+	
+def getReliableData(cols=['HITId','HITTypeId','Title','Description','Keywords','Reward',
+	'CreationTime','MaxAssignments','RequesterAnnotation','AssignmentDurationInSeconds',
+	'AutoApprovalDelayInSeconds','Expiration','NumberOfSimilarHITs','LifetimeInSeconds',
+	'AssignmentId','WorkerId','AssignmentStatus','AcceptTime','SubmitTime','AutoApprovalTime',
+	'ApprovalTime','RejectionTime','RequesterFeedback','WorkTimeInSeconds','LifetimeApprovalRate',
+	'Last30DaysApprovalRate','Last7DaysApprovalRate','Input.pv_id','Input.global_user_id',
+	'Input.time','Input.declaration','Answer.Q1','Approve','Reject']):
+
+	dataSet = qbRel.analyseWorkers();
+	badEntries = qbRel.pickBadEntries(dataSet)
+
+	# print dataSet
+
+	dataSet = dataSet.drop(badEntries.index)
+
+	dataSet = dataSet[['WorkerId','Input.declaration','Answer.Q1']]
+	silverSet = dataSet['Input.declaration']
+
+	# print silverSet
+
+	# load the fulDataset
+
+	paths = qbPre.listFiles(qbGbl.oriFileName)
+	filData = qbPre.readFiles(paths)
+
+	filData = filData[cols];
+
+	filData.index = (xrange(0,len(filData)))
+
+	# remove the observations that were verified earlier as the silver set 
+	dups = pd.DataFrame()
+	for dec in silverSet:
+		# dups = dups.append(filData[filData['Input.declaration']==dec])
+		filData = filData.drop(filData[filData['Input.declaration']==dec].index)
+	
+	badEntries = qbRel.pickBadObs(filData)
+
+	filData = filData.drop(badEntries.index)
+	
+	dataSet = dataSet.append(filData)
+	dataSet = dataSet[['WorkerId','Input.declaration','Answer.Q1']]
+	dataSet.index = (xrange(0,len(dataSet)))
+
+	# print dataSet
+	dataSet.to_csv(qbGbl.finalReaderFile,header=False);
+
 
 def doReliabilityScoring():
 	# load the filtered dataset
@@ -73,7 +189,7 @@ def doObsComplexityScoring():
 ## this funciton tokenizes the words
 def prepareData(type):
 	# load the filtered dataset
-	filData = qbPre.importFilCSV(qbGbl.filFileName,False);
+	filData = qbPre.importFilCSV(qbGbl.finalReaderFile,False);
 	
 	filData = qbPre.prepareData(filData,type);
 	filData = pd.DataFrame(filData,columns=['index','declaration','answer'])
@@ -123,7 +239,7 @@ def preProcessData(type):
 	return [filData,X,Y] 
 
 ## this function takes X and Y and does everything necessary to classify them
-def classifyData(X,Y,C=[1.0]):
+def classifyData(X,Y,C=[0.7]):
 	XTrain, XTest, YTrain, YTest = qbPrepare.segmentData(X,Y,0.4);
 
 	kfolds = qbPrepare.kFoldGenerator(XTrain.shape[0],5)
@@ -195,14 +311,26 @@ def analyse(filename):
 
 	# print new['Input.declaration'].nunique()
 	# print len(old['declaration'].unique())
-	filData = pd.Series(old['declaration'].unique())
+	filData = pd.Series(old['declaration']).drop_duplicates()
+	print filData
+	# tempSer = pd.Series(new['Input.declaration']).drop_duplicates()
+	tempSer = new.drop_duplicates(cols=['Input.declaration'])
+			
+	print len (new)
+	print len(tempSer)
+
 	# print len(filData)
 	# print '================='
 	accuracy = []
 	count=0;
+	# print len(new)
+	# print len(old)
+
 	for row in filData:
 		# print row
 		if not (new[new['Input.declaration'] == row].empty):
+			if len(new[new['Input.declaration'] == row])>1:
+				print new[new['Input.declaration'] == row]
 			count += len(new[new['Input.declaration'] == row])
 			tempOld = qbPre.convClasses(list(old[old['declaration'] == row]['answer'])[0],'|')
 			# print tempOld
@@ -218,9 +346,11 @@ def analyse(filename):
 	
 	print scipy.stats.tmean(accuracy) 
 
+	print count
+
 	P.figure();
 
-	n, bins, patches = P.hist(accuracy,len(set(accuracy)), histtype='bar',cumulative=False)
+	n, bins, patches = P.hist(accuracy,len(set(accuracy)), histtype='step',cumulative=True,normed=1)
 
 	P.title("Score distribution")
 	P.xlabel("score")
@@ -266,6 +396,14 @@ def cleanExistingData(filename1,filename2):
 	# print count
 	# # write = 'data/write/cleanedDataset.csv';
 
+
+
+def filterNoneObs(type):
+	filData = qbPre.readDataFrame('{0}_{1}.csv'.format(qbGbl.dataSetFileName,type),None,0);
+	print filData
+
+
+
 ########################## Main Script ########################
 
 ## filter the data from the main dataset and write the only relevant files to new file
@@ -280,28 +418,65 @@ def cleanExistingData(filename1,filename2):
 # qbRel.writeScorecard(qbGbl.scoreFileName,scoreCard);
 
 # generateNewData()
-types = ['100','110','111']
-# start tokenizing the stuff
-for t in types:
-	type = t;
-	# prepareData(type);
+# types = ['100','110','111']
+# # start tokenizing the stuff
+# for t in types:
+# 	type = t;
+# 	# prepareData(type);
 
-	# qbRel.goldenSet(100);
+# # 	# qbRel.goldenSet(100);
 
-	# ## carry out word scoring and related statistics
-	# initData, dataX, dataY = preProcessData(type);
-	print '\n'
-	print 'Classitying data using text preProcessing specification {0}'.format(type)
-	print '\n'
-	filData,X,Y = preProcessData(type);
+# 	# ## carry out word scoring and related statistics
+# 	# initData, dataX, dataY = preProcessData(type);
+# 	print '\n'
+# 	print 'Classitying data using text preProcessing specification {0}'.format(type)
+# 	print '\n'
+# 	filData,X,Y = preProcessData(type);
 
-	# print Y
-	# scores = cv.cross_val_score(qbPrepare.classifier,X,Y,cv=10,scoring='precision')
+# 	# print Y
+# 	# scores = cv.cross_val_score(qbPrepare.classifier,X,Y,cv=10,scoring='precision')
 
-	# test.testingSVM()
-	classifyData(X,Y,C=[0.1,0.5,0.7,1.0,1.2,2.0])
-	print '\n'
-
-# analyse('{0}/Batch_1189077_batch_results.csv'.format(qbGbl.oriFileName))
+# 	# test.testingSVM()
+# 	classifyData(X,Y,C=[0.1,0.5,0.7,1.0,1.2,2.0])
+# 	print '\n'
+	
+# analyse('{0}/Batch_1191444_batch_results.csv'.format(qbGbl.oriFileName))
 
 # cleanExistingData('{0}/Batch_1189077_batch_results.csv'.format(qbGbl.oriFileName),'{0}/seededfeedback.clean.txt'.format(qbGbl.oriFileName))
+
+
+############################## filter data and do the classification process ####################################
+
+# filData = filterData2(cols=['SubmitTime','WorkerId','Input.declaration','Answer.Q1'])
+
+# types = ['100','110','111']
+# types = ['111']
+
+# start tokenizing the stuff
+# for t in types:
+# 	type = t;
+# 	# prepareData(type);
+
+# 		# print '\n'
+# 	print 'Classitying data using text preProcessing specification {0}'.format(type)
+# 	print '\n'
+# 	filData,X,Y = preProcessData(type);
+
+# 	# print Y
+# 	# scores = cv.cross_val_score(qbPrepare.classifier,X,Y,cv=10,scoring='precision')
+
+# 	# test.testingSVM()
+
+
+# 	# classifyData(X,Y,C=[0.1,0.5,0.7,1.0,1.2,2.0])
+# 	classifyData(X,Y)
+# 	print '\n'
+
+########################## New topic detection in the observations ###########################################
+
+# types = ['100','110','111']
+types = ['100']
+
+for type in types:
+	filterNoneObs(type);
+	
